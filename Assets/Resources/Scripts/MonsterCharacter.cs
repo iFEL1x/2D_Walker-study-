94b348fc52fc0178a63a3239fad70c135e170afa
iFEL1x <86793773+iFEL1x@users.noreply.github.com>
2022-07-18 14:12:13 +0300

    Add Enemy Patrol
    Added script and plugin navMeshPlus.
    Created new Enemy "Enemy Patrol", it is AI wich patrol radius zone and collide player.

---
+++ Assets/Resources/Scripts/MonsterCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MonsterCharacter : NonPlayerCharacter
+{
+    private Animator animator;
+
+    public override void Start()
+    {
+        animator = GetComponent<Animator>();
+        base.Start();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log($"Столкновение {gameObject.name} c {collision.name}");
+            animator.SetTrigger("TakenDamage");
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/NonPlayerCharacter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public abstract class NonPlayerCharacter : MonoBehaviour
+{
+    [Tooltip("Объект зоны патрулирования")]
+    [SerializeField] public Transform patrolZoneCenter;
+
+    private NavMeshHit navMeshHit;
+    private float randomPointRadius;
+    private NavMeshPath navMeshPath; //Путь.
+    private NavMeshAgent agent;
+    private Vector2 randomPoin; //Случайная точка, созданная в заданном радиусе.
+
+
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        navMeshPath = new NavMeshPath();
+        randomPointRadius = patrolZoneCenter.GetComponent<CircleCollider2D>().radius;
+
+        //При использовании NavMeshPlus с Tailmap, данные параметры необходимо отключать.
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        NavRandomPoint();
+    }
+
+    public virtual void Update()
+    {
+        if (agent.remainingDistance == 0)
+        {
+            NavRandomPoint();
+        }
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        bool getCurrentPoint = false;
+
+        while (!getCurrentPoint)
+        {
+            agent.CalculatePath(randomPoin, navMeshPath); //Высчитываем путь до точки
+            if (navMeshPath.status == NavMeshPathStatus.PathComplete) //Если расстояние до точки существует...
+                getCurrentPoint = true;
+        }
+        agent.SetDestination(randomPoin); //...следуем до неё
+    }
+
+    public void NavRandomPoint() //Получаем случайную точку, в заданном радиусе
+    {
+        NavMesh.SamplePosition(Random.insideUnitSphere * randomPointRadius + patrolZoneCenter.position, out navMeshHit, 5f, NavMesh.AllAreas);
+        randomPoin = navMeshHit.position;
+    }
+}
+
+
